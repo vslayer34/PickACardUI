4c010eeac010b02c91130685fe1e2bf38077e641
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-06-24 02:38:03 +0300

    Add project files.

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PickACardUI
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = GetRandomValue() + " of " + GetRandomSuit();
+            }
+
+            return pickedCards;
+        }
+
+        private static string GetRandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) { return "Ace"; }
+            if (value == 11) { return "Jack"; }
+            if (value == 12) { return "Queen"; }
+            if (value == 13) { return "King"; }
+            
+            return value.ToString();
+        }
+
+        private static string GetRandomSuit()
+        {
+            int value = random.Next(1, 5);
+
+            if (value == 1) { return "Spades"; }
+            if (value == 2) { return "Hearts"; }
+            if (value == 3) { return "Clubs"; }
+            
+            return "Diamonds";
+        }
+    }
+}
